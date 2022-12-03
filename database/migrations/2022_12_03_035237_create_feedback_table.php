2d203f82f8461380e729a30bf5d9997e2c44d627
MaritsIkmalY <maritsikmaly@gmail.com>
2022-12-03 10:56:01 +0700

    migration new

---
+++ database/migrations/2022_12_03_035237_create_feedback_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->constrained('students');
+            $table->foreignId('teacher_id')->constrained('teachers');
+            $table->text('message')->nullable();
+            $table->integer('rate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+};
