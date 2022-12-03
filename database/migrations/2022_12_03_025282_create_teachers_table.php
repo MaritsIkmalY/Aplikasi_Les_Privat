7b3e02ab657a5d94b7fffa5a12b40d8cdaa62f86
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2022-12-03 10:45:36 +0700

    migration-constained

--- database/migrations/2022_12_03_025282_create_teachers_table.php
+++ database/migrations/2022_12_03_025282_create_teachers_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('teachers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('category_id');
             $table->string('name')->nullable();
             $table->string('address')->nullable();
             $table->string('photo_profile')->nullable();
