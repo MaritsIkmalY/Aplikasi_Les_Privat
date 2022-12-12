238feb30b13541ec9ce8d77035ac05db6d4403a7
MaritsIkmalY <maritsikmaly@gmail.com>
2022-12-12 13:21:08 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('/profile/teacher', ProfileTeacherController::class)->only('update');
+    Route::resource('/profile/student', ProfileStudentController::class)->only('update');
 });
 
 require __DIR__ . '/auth.php';
