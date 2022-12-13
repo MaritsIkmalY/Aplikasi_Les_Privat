604b0230d188ff03a210a1eff3bf66b7ba54abf3
MaritsIkmalY <maritsikmaly@gmail.com>
2022-12-13 22:44:07 +0700

    Fix Profile

--- resources/views/profile/profile-index.blade.php
+++ resources/views/profile/profile-index.blade.php
@@ -22,13 +22,7 @@
                 <div class="flex flex-col gap-4 mt-5">
                     <div class="flex justify-between items-center">
                         <h1 class="text-2xl">Biodata</h1>
-<<<<<<< HEAD
                         <a class="btn btn-success" href="{{ route('profile.edit') }}">Edit Biodata</a>
-=======
-                        <a class="btn btn-success" href="{{ route('profile.edit') }}">
-                            Edit Biodata
-                        </a>
->>>>>>> ba9a7db18d9583d4bb0834ee1833641157da155d
                     </div>
                     <p>Nama : {{ Auth::user()->name }}</p>
                     <p>Email : {{ Auth::user()->email }}</p>
@@ -36,52 +30,19 @@
                     <p>Phone : {{ Auth::user()->phone }}</p>
                 </div>
 
-<<<<<<< HEAD
-                <div class="flex flex-col gap-4 mt-5">
-                    <div class="flex justify-between items-center">
-                        <h1 class="text-2xl">Pendidikan</h1>
-                        <button class="btn btn-success">Edit Pendidikan</button>
-                    </div>
-                    @foreach ($teacher->education as $education)
-                        <p>{{ $education->name }} - {{ $education->description }}</p>
-                    @endforeach
-                </div>
-
-
                 <!--pendidikan-->
-                <!--sertif-->
-                <div class="flex flex-col gap-5">
-                    <div class="flex justify-between items-center">
-                        <h1 class="text-2xl">Sertifikat</h1>
-                        <button class="btn btn-success">Edit Sertifikat</button>
-                    </div>
-                    <div class="flex gap-4 items-stretch">
-                        @foreach ($teacher->certificate as $certificate)
-                            <div class="card w-96 bg-base-100 shadow-xl">
-                                <figure><img src="/storage/{{ $certificate->file_path }}" alt="Shoes" /></figure>
-                                <div class="card-body">
-                                    <h2 class="card-title">
-                                        {{ $certificate->name }}
-                                    </h2>
-                                    <p>{{ $certificate->description }}</p>
-                                </div>
-                            </div>
-=======
                 @if (Auth::user()->role_id == 2)
-                    {{-- Pendidikan --}}
                     <div class="flex flex-col gap-4 mt-5">
                         <div class="flex justify-between items-center">
                             <h1 class="text-2xl">Pendidikan</h1>
-                            <a href={{ route('education.index') }} class="btn btn-success">Edit Pendidikan</a>
+                            <a href="{{ route('education.index') }}" class="btn btn-success">Edit Pendidikan</a>
                         </div>
                         @foreach ($teacher->education as $education)
                             <p>{{ $education->name }} - {{ $education->description }}</p>
->>>>>>> ba9a7db18d9583d4bb0834ee1833641157da155d
                         @endforeach
                     </div>
 
 
-                    <!--pendidikan-->
                     <!--sertif-->
                     <div class="flex flex-col gap-5">
                         <div class="flex justify-between items-center">
@@ -102,7 +63,6 @@
                                 </div>
                             @endforeach
                         </div>
-                    </div>
                 @endif
             </div>
         </div>
