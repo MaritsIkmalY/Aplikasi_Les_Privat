7000061de8520d184578127225f3ae59428e3eab
MaritsIkmalY <maritsikmaly@gmail.com>
2022-12-15 11:30:32 +0700

    Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,7 +11,7 @@
 
             <!-- Name -->
             <div>
-                <x-input-label for="username" :value="__('Username')" />
+                <x-input-label for="username" :value="__('Nama Pengguna')" />
                 <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')"
                     required autofocus />
                 <x-input-error :messages="$errors->get('username')" class="mt-2" />
@@ -27,11 +27,11 @@
 
             <!--Role-->
             <div class="mt-4">
-                <x-input-label for="role" :value="__('Role')" />
+                <x-input-label for="role" :value="__('Peran')" />
                 <select id="role" name="role" class="select select-bordered w-full max-w-xs">
-                    <option disabled selected>Pick Role</option>
-                    <option value="1">Student</option>
-                    <option value="2">Teacher</option>
+                    <option disabled selected>Pilih Peran</option>
+                    <option value="1">Murid</option>
+                    <option value="2">Pengajar</option>
                 </select>
                 <x-input-error :messages="$errors->get('role')" class="mt-2" />
             </div>
@@ -39,7 +39,7 @@
 
             <!-- Password -->
             <div class="mt-4">
-                <x-input-label for="password" :value="__('Password')" />
+                <x-input-label for="password" :value="__('Kata Sandi')" />
 
                 <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
                     autocomplete="new-password" />
@@ -49,7 +49,7 @@
 
             <!-- Confirm Password -->
             <div class="mt-4">
-                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+                <x-input-label for="password_confirmation" :value="__('Konfirmasi Kata Sandi')" />
 
                 <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
                     name="password_confirmation" required />
@@ -60,11 +60,11 @@
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Sudah daftar?') }}
                 </a>
 
                 <x-primary-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Daftar') }}
                 </x-primary-button>
             </div>
         </form>
